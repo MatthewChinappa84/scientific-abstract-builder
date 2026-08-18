b698a86c8eb40a2f244ca8471f80f9024e692728
Matthew <matthew2025@MattC.local>
2026-08-18 18:25:14 +1200

    Configure browser packages for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  serverExternalPackages: [
+    "playwright-core",
+    "@sparticuz/chromium"
+  ],
 };
+
 export default nextConfig;
